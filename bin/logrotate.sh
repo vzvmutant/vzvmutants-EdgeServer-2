344c151dbce3748c14743158262d398ca4ae4977
vzvmutant <loranmccormick@icloud.com>
2026-01-08 18:25:40 -0600

    Update logrotate.sh

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -13,8 +13,8 @@ DEST="$ARCHIVE/$YEAR/$MONTH/$DAY"
 # Ensure archive directory exists
 mkdir -p "$DEST"
 
-# Move /var/log/messages and /var/log/messages.0 if they exist
-for f in /var/log/messages /var/log/messages.0; do
+# Move /opt/var/log/messages and /opt/var/log/messages.0 if they exist
+for f in /opt/var/log/messages /opt/var/log/messages.0; do
     [ -f "$f" ] || continue
     mv "$f" "$DEST"/
 done
